a7122156d00618d7b2fc9c7fcd22ebf547af9379
wdturner <wdturner@gmail.com>
2022-03-21 15:28:16 -0400

    Removing the changes accidently checked in from class examples.

--- Modules/08.TestingAndCI/test_um_unittest.py
+++ Modules/08.TestingAndCI/test_um_unittest.py
@@ -6,11 +6,11 @@ class TestUM(unittest.TestCase):
          def setUp(self):
              pass
                   
-         def test_numbers_3_4(self):
+         def test_numbers_5_4(self):
              self.assertEqual( multiply(5,4), 20)
                                   
          def test_strings_a_3(self):
-             self.assertEqual( multiply('a',4), 'aaaa')
+             self.assertEqual( multiply('a',3), 'aaa')
     
 if __name__ == '__main__':
     unittest.main()
