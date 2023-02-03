ace3acad30962918d26d30851b4d236ffc2d9cdf
chengyuzz <chengyuz0921@gmail.com>
2023-02-03 17:31:25 -0500

    Change the link

--- Website/generateSyllabus.js
+++
@@ -1,7 +0,0 @@
-var marked = require('marked');
-var fs = require('fs');
-
-var syllabusMD = fs.readFileSync('Syllabus.md', 'utf-8');
-var markdownSyllabus = marked(syllabusMD);
-
-fs.writeFileSync('./site/syllabus.html', markdownSyllabus);
