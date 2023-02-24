78bdf2a3f2750b411f694ce6e30f6102e4092446
“Chengyu <97137422+gnehcuyz@users.noreply.github.com>
2023-02-24 18:31:02 -0500

    Add a button to expand the meun

--- WebpageReact/src/components/navBar.jsx
+++ WebpageReact/src/components/navBar.jsx
@@ -1,25 +1,37 @@
+import { Component } from 'react';
 import logo from '../images/logo.svg';
 import "../styles/NavBarStyle.css";
 
-function NavBar() {
-    return (
-       <>
-        <nav>
-            <a href="index.html">
-            <img src={logo} width={100} height={100} />
-            </a>
-        
-            <div>
-                <ul id="navbar">
-                    <li><a href="index.html">Home</a></li>
-                    <li><a href="index.html">Syllabus</a></li>
-                    <li><a href="index.html">Modules</a></li>
-                    <li><a href="index.html">Discussion forum</a></li>
-                </ul>
-            </div>
-        </nav>
-       </>
-    )
+class NavBar extends Component {
+    state={clicked: false};
+    handleClick = () =>{
+        this.setState({clicked: !this.state.clicked})
+    }
+
+    render() {
+        return (
+        <>
+            <nav>
+                <a href="index.html">
+                <img src={logo} width={100} height={100} />
+                </a>
+            
+                <div>
+                    <ul id="navbar">
+                        <li><a className="active" href="index.html">Home</a></li>
+                        <li><a href="index.html">Syllabus</a></li>
+                        <li><a href="index.html">Modules</a></li>
+                        <li><a href="index.html">Discussion forum</a></li>
+                    </ul>
+                </div>
+
+                <div id="mobile" onClick={this.handleClick}>
+                    <i id="bar" className={this.state.clicked?"fas fa-times" : "fas fa-bars"}></i>
+                </div>
+            </nav>
+        </>
+        )
+    }
 }
 
 export default NavBar;
