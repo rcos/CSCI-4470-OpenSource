4ae7c3fbd288cce68b639cbdc3697efc6c613215
“Chengyu <97137422+gnehcuyz@users.noreply.github.com>
2023-03-21 14:58:38 -0400

    Finish responsive navBar CSS work

--- WebpageReact/src/components/navBar.jsx
+++ WebpageReact/src/components/navBar.jsx
@@ -5,7 +5,7 @@ import "../styles/NavBarStyle.css";
 import { MenuItems } from './Menuitems';
 
 class NavBar extends Component {
-    state={clicked: false};
+    state = {clicked: false};
     handleClick = () =>{
         this.setState({clicked: !this.state.clicked})
     }
@@ -13,22 +13,29 @@ class NavBar extends Component {
     render() {
         return (
             <nav className="NavBarItems">
-                <img src={logo} width={100} height={60}/>
-                <h1 className="navbar-logo" style={{ fontSize: 35 }}>CSCI4470 OSS</h1>
-                  <ul className='nav-menu'>
-                    {
-                        MenuItems.map((item, index) => {
-                            return (
-                                <li key={index}>
-                                    <a href="/">
-                                        <i className={item.icon}></i>
-                                        {item.title}
-                                    </a>
-                                </li>
-                            );
-                        })
-                    }
-                  </ul>
+                <img src={logo} width={140} height={60}/>
+                <h1 className="navbar-logo"> CSCI4470 Open Source Software</h1>
+
+                <div className='menu-icons' onClick={this.handleClick}>
+                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'} 
+                    onClick={this.handleClick}>
+                    </i>
+                </div>
+
+                <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+                {
+                    MenuItems.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <a className={item.cName} href="/">
+                                    <i className={item.icon}></i>
+                                    {item.title}
+                                </a>
+                            </li>
+                        );
+                    })
+                }
+                </ul>
             </nav>
         )
     }
