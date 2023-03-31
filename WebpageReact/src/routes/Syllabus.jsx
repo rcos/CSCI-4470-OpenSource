1698903ccade2ebadd3c97ee2c442d2e08882b8c
Swagmoneydre69 <lungua0@gmail.com>
2023-03-31 16:48:32 -0400

    Updated Syllabus page to display Syllabus.md markdown file Displaying text here instead of putting Syllabus.md display in its own component to better follow react conventions

--- WebpageReact/src/routes/Syllabus.jsx
+++ WebpageReact/src/routes/Syllabus.jsx
@@ -1,14 +1,28 @@
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
+/* react package that displays markdown text
+ * NOTE - need to have react-markdown installed 
+ * for this to work: do npm install react-markdown
+ */
+import ReactMarkdown from 'react-markdown';
 
 function Syllabus () {
-    return (
-      <>
-        <NavBar />
-        <h1>something</h1>
-        <Footer />
-      </>
-    )
+  // load text from Syllabus.md into a string
+  // to be displayed on the site
+  const syllabus = '';
+  useEffect(() => {
+    fetch("../../../Syllabus.md")
+      .then(response => response.text())
+      .then(text => {syllabus = text});
+  }, []);
+
+  return (
+    <>
+      <NavBar />
+      <ReactMarkdown source={syllabus}/>
+      <Footer />
+    </>
+  )
   }
   
 export default Syllabus;
