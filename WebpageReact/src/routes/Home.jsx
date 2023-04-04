a8992f72609447892517870f5351c9a93ab6cd5c
“Chengyu <97137422+gnehcuyz@users.noreply.github.com>
2023-04-04 16:40:24 -0400

    Develop Homepage

--- WebpageReact/src/routes/Home.jsx
+++ WebpageReact/src/routes/Home.jsx
@@ -1,31 +1,12 @@
-import Page from "../components/Page";
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
-import '../styles/Footer.css';
+import HeroImg from '../components/HeroImg';
+import '../styles/Home.css';
 
 function Home () {
   return (
     <>
-      <NavBar />
-      <Page 
-        cName="page"
-        title="Open Source Software"
-        text="Unlocking Innovation with Open Source"
-      />
-      <div className="course">
-        <h3>Course Description</h3>
-        <p>
-          This course will introduce students to the world of open source software.
-          Students will learn about the history of open source software, the
-          benefits of open source software, and the different types of open source
-          licenses. Students will also learn about the different types of open source
-          projects and how to contribute to open source projects. Students will
-          participate in a semester long open source project and will learn about
-          the different roles in open source projects. 
-        </p>
-        
-      </div>
-      <Footer />
+      <HeroImg />
     </>
   )
 }
