cd63c7c8c7962caae34912f419e5abb8ced9a58f
Swagmoneydre69 <lungua0@gmail.com>
2023-04-06 18:34:46 -0400

    Updated documentation for Module.jsx page

--- WebpageReact/src/routes/Modules.jsx
+++ WebpageReact/src/routes/Modules.jsx
@@ -1,15 +1,22 @@
 import '../styles/Footer.css';
-import '../styles/ModuleStyle.css'
-import data from '../JSONfile/ModulesData.json';
+import '../styles/ModuleStyle.css';
+// json file containing links for each module
+import moduleData from '../JSONfile/ModulesData.json';
 
 function Modules () {
+    /* when a button for the module is clicked, it should collapse
+     * i.e. the height of the following content div should 
+     * switch between 0 and its nonzero scrollHeight
+     */
     const collapse = (event) =>{
         const content = event.currentTarget.nextElementSibling;
         content.style.maxHeight = (content.style.maxHeight) ? null : content.scrollHeight + "px";
     }
+    // renders a button and a collapsible list of important
+    // links/files for each module in moduleData using mapping
     return (
       <>
-      {data.map(item =>{
+      {moduleData.map(item =>{
         return(
         <>
             <button className='collapsible'onClick={collapse}>{item.title}</button>
@@ -27,6 +34,6 @@ function Modules () {
       })}
       </>
     )
-  }
+}
   
 export default Modules;
