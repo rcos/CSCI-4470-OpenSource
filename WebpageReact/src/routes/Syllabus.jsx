d2838914905d24dd0899ac4928e558a6d499a8f1
“Chengyu <97137422+gnehcuyz@users.noreply.github.com>
2023-04-07 17:04:13 -0400

    Continue develop Syllabus.jsx

--- WebpageReact/src/routes/Syllabus.jsx
+++ WebpageReact/src/routes/Syllabus.jsx
@@ -1,35 +1,21 @@
-import React, { useState, useEffect } from "react";
-import ReactMarkdown from "react-markdown";
-/* react package that displays markdown text
- * NOTE - need to have react-markdown installed 
- * for this to work: do npm install react-markdown
- */
-import Markdown from 'markdown-to-jsx';
-import syllabusMD from '../../Syllabus.md';
+import React, { useEffect, useState } from "react";
+import syllabus from "../../../Modules/01.Introduction/Syllabus/Syllabus.md";
 
-class Syllabus extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { md: "" }
-  }
 
-  componentWillMount() {
-    fetch(syllabusMD)
-      .then((res) => res.text())
-      .then((md) => {
-        this.setState({ md })
-      })
-  }
+const Syllabus = () => {
+  const [post, setPost] = useState("");
+  useEffect(() => {
+      fetch(syllabus)
+        .then((res) => res.text())
+        .then((response) => setPost(response))
+        .catch((err) => setPost(err));
+    });
 
-  render() {
-    let { md } = this.state
-
-    return (
-      <div className="post">
-        <Markdown children={md}/>
-      </div>
-    )
-  }
-}
+  return (
+    <div className="container">
+      <Markdown>{post}</Markdown>
+    </div>
+  );
+};
 
 export default Syllabus;
