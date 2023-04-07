b6eccbb37edd39713c4ab70b02f62f2a3e487a69
Swagmoneydre69 <lungua0@gmail.com>
2023-04-07 17:13:56 -0400

    Implemented collapsible list of course resources

--- WebpageReact/src/routes/Resources.jsx
+++ WebpageReact/src/routes/Resources.jsx
@@ -1,7 +1,30 @@
+import '../styles/ModuleStyle.css';
+import resourceData from '../JSONfile/ResourcesData.json';
+
 function Resources () {
+  const collapse = (event) =>{
+    const content = event.currentTarget.nextElementSibling;
+    content.style.maxHeight = (content.style.maxHeight) ? null : content.scrollHeight + "px";
+  }
+  // renders collapsible list of course resources
     return (
       <>
-        <h1>something</h1>
+        <h1>Course Resources</h1>
+        {resourceData.map(item =>{
+          return(
+            <>
+              <button className='collapsible'onClick={collapse}>{item.title}</button>
+              <div className='content'></div>
+              <ul>
+                {item.links.map(link =>{
+                  return(
+                    <li><a href={link.link}>{link.linkName}</a></li>
+                  );
+                })}
+              </ul>
+            </>
+          )
+        })}
       </>
       
     )
