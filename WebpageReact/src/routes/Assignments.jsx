041435a02a5584dd6668185c29705aee3081c3b2
“Chengyu <97137422+gnehcuyz@users.noreply.github.com>
2023-04-11 16:33:49 -0400

    Develop AnalysisHW

--- WebpageReact/src/routes/Assignments.jsx
+++ WebpageReact/src/routes/Assignments.jsx
@@ -1,14 +1,44 @@
-import NavBar from '../components/NavBar';
-import Footer from '../components/Footer';
-import React, { useState } from "react";
-import '../styles/Assignments.css';
+import AnalysisHW from '../JSONfile/AnalysisHW.json';
+import TermProject from '../JSONfile/TeamProject.json';
 
-function Assignments () {
+function Assignment () {
+    /* when a button for the module is clicked, it should collapse
+     * i.e. the height of the following content div should 
+     * switch between 0 and its nonzero scrollHeight
+     */
+    const collapse = (event) =>{
+        const content = event.currentTarget.nextElementSibling;
+        content.style.maxHeight = (content.style.maxHeight) ? null : content.scrollHeight + "px";
+    }
     return (
       <>
+        <div className='AnalysisHW'>
+            <button className='collapsible'onClick={collapse}>Analysis Homework</button>
+            <div className='content'>
+                <ul>
+                    {AnalysisHW.map(item => {
+                        return(
+                          <li><a href={item.link}>{item.title}</a></li>
+                        );
+                    })}
+                </ul>
+            </div>
+        </div>
 
+        <div className='TermWork'>
+            <button className='collapsible'onClick={collapse}>Term Project</button>
+            <div className='content'>
+                <ul>
+                    {TermProject.map(item => {
+                        return(
+                          <li><a href={item.link}>{item.title}</a></li>
+                        );
+                    })}
+                </ul>
+            </div>
+        </div>
       </>
     )
 }
   
-  export default Assignments;
+export default Assignment;
