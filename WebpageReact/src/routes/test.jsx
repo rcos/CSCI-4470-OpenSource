90b4f2fddd707e0d0ad2ad5d6f868dbfec3957b1
“Chengyu <97137422+gnehcuyz@users.noreply.github.com>
2023-04-11 19:17:30 -0400

    Render Assignment

--- WebpageReact/src/routes/test.jsx
+++ WebpageReact/src/routes/test.jsx
@@ -1,16 +1,37 @@
-import NavBar from '../components/NavBar';
-import Footer from '../components/Footer';
-import HeroImg from '../components/HeroImg';
-import '../styles/Home.css';
 
-function Test () {
+import React from 'react';
+import { Accordion, Card, Button } from 'react-bootstrap';
+import '../styles/ModuleStyle.css';
+// json file containing links for each module
+import data from '../JSONfile/ModulesData.json';
+
+const Modules = () => {
   return (
-    <>
-      <NavBar />
-        <h1>Modules</h1>
-      <Footer />
-    </>
-  )
-}
+    <Accordion>
+      {data.map((module, index) => (
+        <Card key={index}>
+          <Card.Header>
+            <h1>Something</h1>
+            <Accordion.Toggle as={Button} variant="link" eventKey={index}>
+              {module.title}
+            </Accordion.Toggle>
+          </Card.Header>
 
-export default Test;
+          <Accordion.Collapse eventKey={index}>
+            <Card.Body>
+              <ul>
+                {module.links.map((link, linkIndex) => (
+                  <li key={linkIndex}>
+                    <a href={link.link}>{link.linkName}</a>
+                  </li>
+                ))}
+              </ul>
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      ))}
+    </Accordion>
+  );
+};
+
+export default Modules;
