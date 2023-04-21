71c0484c3d72d1647a7263fe910fc2c27c399d27
Swagmoneydre69 <lungua0@gmail.com>
2023-04-21 16:54:31 -0400

    Added another OSS image to the homepage from the course resources

--- WebpageReact/src/routes/Home.jsx
+++ WebpageReact/src/routes/Home.jsx
@@ -1,6 +1,6 @@
-//import HeroImg from '../components/HeroImg';
 import '../styles/Home.css';
-import RPI1 from "../assets/lab1_1.jpg";
+import img1 from "../assets/lab1_1.jpg";
+import img2 from "../assets/lab10_2.jpg";
 import React from "react";
 
 function Home () {
@@ -11,7 +11,10 @@ function Home () {
       <p>Open Source has been taught at RPI since Fall of 2007 <br></br>(taught jointly by Dr. William Shroeder and Dr. Luis Ibanez)
          as a 4000 level class, and then by Mukkai Krishnamoorthy as a 2000 level class. <br></br>We will continue this tradition by 
          offering it as a 4000 level course.</p>
-      <img className="into-img" src={RPI1} alt="RPI1"/>
+      <div className='imgs'>
+      <img className="into-img" src={img1} alt="img1"/>
+      <img className="into-img" src={img2} alt="img2"/>
+      </div>
       <div className="content">
         <h1>Open Source Software</h1>
         <h2>Unlocking Innovation with Open Source</h2>
