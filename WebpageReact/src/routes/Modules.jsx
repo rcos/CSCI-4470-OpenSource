e8999bc988d3c771dafa713c8a4accdcf4eefa9d
Erica Nazimowitz <nazime@rpi.edu>
2023-08-04 15:47:59 -0400

    Overhauled Modules.jsx
    Modules.jsx now uses the React-collapsible feature, which is a lot nicer and smoother.

--- WebpageReact/src/routes/Modules.jsx
+++ WebpageReact/src/routes/Modules.jsx
@@ -1,44 +1,27 @@
 import '../styles/ModuleStyle.css';
 // json file containing links for each module
 import moduleData from '../JSONfile/ModulesData.json';
+import React from 'react';
+import Collapsible from 'react-collapsible';
 
 function Modules () {
-    /* when a button for the module is clicked, it should collapse
-     * i.e. the height of the following content div should 
-     * switch between 0 and its nonzero scrollHeight
-     */
-    const collapse = (event) =>{
-        const content = event.currentTarget.nextElementSibling;
-        content.style.maxHeight = (content.style.maxHeight) ? null : content.scrollHeight + "px";
-    }
-    // renders a button and a collapsible list of important
-    // links/files for each module in moduleData using mapping
     return (
-      <>
-      <h3>Course Modules </h3>
+    <>
+    <h3>Course Modules </h3>
       <p>Here are the modules for the course. Click on a module for links to important material
-        like labs, slides, and exercises. 
-      </p>
-        <div class="container">
-            {moduleData.map((item) =>{
-                return(
-                <>
-                    <button className='collapsible'onClick={collapse}>{item.title}</button>
-                    <div className='content'>
-                        <ul>
-                            {item.links.map(link => {
-                                return(
-                                    <li><a href={link.link}>{link.linkName}</a></li>
-                                );
-                            })}
-
-                        </ul>
-                    </div>
-                </>
-                )
-            })}
-        </div>
-      </>
+        like labs, slides, and exercises. </p>
+    {moduleData.map((item) => {
+        return (<Collapsible trigger={item.title}>
+                <ul>
+                    {item.links.map(link => {
+                        return(
+                            <li><a href ={link.link}>{link.linkName}</a></li>
+                        );
+                    })}
+                </ul>
+        </Collapsible>)
+    })}
+    </>
     )
 }
   
